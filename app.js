005fc18f4c936d10826071588c9641b5598d46ea
shreyanshs7 <shreyanshss7@gmail.com>
2019-08-10 03:01:47 +0530

    Added API to retrieve transactions

--- app.js
+++ app.js
@@ -3,16 +3,46 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
+var Web3 = require('web3');
+
+const providerUrl = 'kovan.infura.io/v3/6c6f87a10e12438f8fbb7fc7c762b37c';
+
+var web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
 var UserTransaction = require('./models/UserTransaction');
 
 var app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//API to insert user transactions
+app.post('/user/transaction', function(req, res) {
+  var userTransaction = new UserTransaction(req.body).save(function(err) {
+    if (err) res.json(err);
+    res.json(userTransaction);
+  });
+});
+
+//API to retrieve user transactions based on user address which the user has sent to
+app.get('/user/transactions', function(req, res) {
+  console.log("AAAAAA ", req.query);
+  if(req.query.from) {
+    UserTransaction.find({from: req.query.from}, function(err, userTransactions) {
+      if (err) res.json(err);
+      res.json(userTransactions);
+    });
+  } else if (req.query.to) {
+    UserTransaction.find({to: req.query.to}, function(err, userTransactions) {
+      if (err) res.json(err);
+      res.json(userTransactions);
+    });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -21,13 +51,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
 module.exports = app;
--- models/UserTransaction.js
+++ models/UserTransaction.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017');
+mongoose.connect('mongodb://127.0.0.1:27017/mydb', {useNewUrlParser: true});
 
 const Schema = mongoose.Schema;
 
-const UserTransaction = new Schema({
+const UserTransactionSchema = new Schema({
     from: Schema.Types.String,
     to: Schema.Types.String,
     transactionHash: Schema.Types.String,
     blockNumber: Schema.Types.Number 
 });
+
+const UserTransaction = mongoose.model('UserTransaction', UserTransactionSchema);
 
 module.exports = UserTransaction;
